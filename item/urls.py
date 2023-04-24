6c8b6fd1a7eb0158324914440355961c7cf6b4dc
accesoriotoluca <accesoriotoluca@gmail.com>
2023-04-24 07:51:00 -0600

    first commit

---
+++ item/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import items, new, detail, delete, edit
+
+app_name = 'item'
+
+urlpatterns = [
+    path('', items, name='items'),
+    path('new/', new, name='new'),
+    path('<int:pk>', detail, name='detail'),
+    path('<int:pk>/delete/', delete, name='delete'),
+    path('<int:pk>/edit/', edit, name='edit'),
+]
