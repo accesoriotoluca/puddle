666ad898cfe9421e9da1ebf7ca01ab042d8c0169
zeroylalus <armopc8@gmail.com>
2023-04-28 15:14:17 -0600

    una cosilla

--- puddle/urls.py
+++ puddle/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
