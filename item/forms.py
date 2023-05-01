88d98c4bec79e277c96d3ec3fe0c9e0703580c20
accesoriotoluca <accesoriotoluca@gmail.com>
2023-05-01 15:30:20 -0600

    commit

--- item/forms.py
+++ item/forms.py
@@ -3,6 +3,30 @@ from .models import Item
 
 INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
 
+# estos formularios pueden ya tener la lógica de validación de su modelo
+
+"""
+?utiliza Django ModelForms 
+*para crear un formulario basado en un modelo específico. 
+Esto significa que el formulario se creará automáticamente 
+con campos correspondientes al modelo, 
+* y también se generará automáticamente la validación de los datos de entrada 
+* y la creación de objetos del modelo correspondiente. 
+?Este enfoque es útil: 
+cuando se desea una integración completa entre la base de datos y el formulario, 
+y cuando se desea aprovechar la capacidad de Django de crear formularios a partir de modelos.
+
+! si no especificas campos, widgets, validación
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+* Django crea automáticamente campos para cada campo del modelo especificado en el atributo model de la clase Meta.
+* Además, los campos creados tendrán automáticamente los widgets correspondientes a cada tipo de campo del modelo.
+* y se encargará de la validación de los datos ingresados.
+? si deseas personalizar la forma en que se muestran los campos 
+? o cómo se valida la entrada del usuario, 
+? puedes especificar los widgets y la validación manualmente"""
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
